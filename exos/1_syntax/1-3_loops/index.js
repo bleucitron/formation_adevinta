3370e4e647314011ce1cd88cc70a739f3aa0178b
Romain Crestey <rcrestey@gmail.com>
2022-09-21 11:28:13 +0200

    jour 1

--- exos/1_syntax/1-3_loops/index.js
+++ exos/1_syntax/1-3_loops/index.js
@@ -12,6 +12,13 @@ console.log('*** Assurance vie ***');
  * 3) Afficher le résultat
  */
 
+let money = 10000;
+
+for (let i = 0; i <= 30; i += 0.5) {
+  money *= 1.02;
+}
+console.log('money', money);
+
 /**========================================================================
  *                           Paires
  *========================================================================**/
@@ -21,6 +28,12 @@ console.log('*** Paires ***');
  * 1) Affichez toutes les paires possibles de nombres entre 0 et 9.
  * (1, 2) et (2, 1) sont considérées 2 paires différentes.
  */
+
+for (let i = 0; i <= 9; i++) {
+  for (let j = 0; j <= 9; j++) {
+    console.log('I', i, 'J', j);
+  }
+}
 
 /**========================================================================
  *                           Carte Bleue
@@ -35,10 +48,33 @@ console.log('*** Carte bleue ***');
  * 4) Combien d'opérations ont pu être effectuées avant de ne plus pouvoir payer ?
  */
 
+let argent = 1000;
+const cout = 30;
+let nb = 0;
+
+while (argent >= cout) {
+  argent -= 30;
+  nb++;
+}
+
+console.log('Argent restant', argent);
+console.log('Nb', nb);
+
 /**
  * Bonus 1) Comment retrouver le résultat précédent avec une boucle for ?
  * Bonus 2) Comment retrouver ce résultat précédent sans aucune boucle ?
  */
+let argent2 = 1000;
+const cout2 = 30;
+let nb2 = 0;
+
+for (let money2 = 1000; money2 >= cout; money2 -= cout2) {
+  nb2++;
+}
+console.log('Argent restant', argent2);
+console.log('Nb', nb2);
+
+console.log(Math.trunc(1000 / 30));
 
 /**========================================================================
  *                           [Bonus] Mastercard
